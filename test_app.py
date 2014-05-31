456bec9d7d6e1a5f94e1349fce9b953f1d779df3
Vasilis Nicolaou <vaslabs@yahoo.gr>
2014-05-31 09:18:53 +0100

    Fixed error when daemon changes directory: templates are not found

--- test_app.py
+++ test_app.py
@@ -1,13 +1,13 @@
+import os
 from flask import Flask, render_template
 from test_framework import run_tests
 import json
-
-app = Flask(__name__)
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__, template_folder=tmpl_dir)
 
 @app.route("/")
 def run_test():
-    action_times = run_tests("config.cfg", "https://192.168.56.101:8003")
-    
+    action_times = run_tests("config.cfg", "https://192.168.2.10:8003")
     return render_template('results.html', action_times=json.dumps(action_times))
     
 
